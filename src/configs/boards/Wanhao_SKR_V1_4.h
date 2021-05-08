4004d7131b12336f8a0cb36b8e78baa7c1f6a444
nvangeffen <neilvangeffen@gmail.com>
2021-05-09 00:18:26 +1200

    back to more default, and edit duplicator config file

--- src/configs/boards/Wanhao_SKR_V1_4.h
+++ src/configs/boards/Wanhao_SKR_V1_4.h
@@ -55,16 +55,40 @@
 // Limit Switches
 //
 
-#define X_MAX_PIN							X_DIAG_PIN
-#define Y_MAX_PIN							Y_DIAG_PIN
-#define Z_MAX_PIN							Z_DIAG_PIN
-#define X_MIN_PIN							E0_DIAG_PIN
-#define Y_MIN_PIN							E1_DIAG_PIN
-#define Z_MIN_PIN							PWR_DET_PIN
+#ifdef X_STALL_SENSITIVITY
+  #define X_STOP_PIN                  X_DIAG_PIN
+  #if X_HOME_DIR < 0
+    #define X_MAX_PIN                      P1_26  // E0DET
+  #else
+    #define X_MIN_PIN                      P1_26  // E0DET
+  #endif
+#else
+  #define X_STOP_PIN                       P1_29  // X-STOP
+#endif
 
-#define X_STOP_PIN							X_MAX_PIN
-#define Y_STOP_PIN							Y_MAX_PIN
-#define Z_STOP_PIN							Z_MAX_PIN
+#ifdef Y_STALL_SENSITIVITY
+  #define Y_STOP_PIN                  Y_DIAG_PIN
+  #if Y_HOME_DIR < 0
+    #define Y_MAX_PIN                      P1_25  // E1DET
+  #else
+    #define Y_MIN_PIN                      P1_25  // E1DET
+  #endif
+#else
+  #define Y_STOP_PIN                       P1_28  // Y-STOP
+#endif
+
+#ifdef Z_STALL_SENSITIVITY
+  #define Z_STOP_PIN                  Z_DIAG_PIN
+  #if Z_HOME_DIR < 0
+    #define Z_MAX_PIN                      P1_00  // PWRDET
+  #else
+    #define Z_MIN_PIN                      P1_00  // PWRDET
+  #endif
+#else
+  #ifndef Z_STOP_PIN
+    #define Z_STOP_PIN                     P1_27  // Z-STOP
+  #endif
+#endif
 
 //
 // Z Probe (when not Z_MIN_PIN)
